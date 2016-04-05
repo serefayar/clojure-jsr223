b61d856818cda16b80a93182e1d299eef4451315
Şeref R. Ayar <seref.ayar@btc-ag.com.tr>
2016-04-05 21:37:46 +0300

    basic changes to compile clojure

--- src/clojure/contrib/jsr223/ClojureScriptEngine.java
+++ src/clojure/contrib/jsr223/ClojureScriptEngine.java
@@ -1,52 +1,32 @@
 /*
  * Copyright (c) 2009 Armando Blancas. All rights reserved.
- * 
+ *
  * The use and distribution terms for this software are covered by the
  * Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
  * which can be found in the file epl-v10.html at the root of this distribution.
- * 
+ *
  * By using this software in any fashion, you are agreeing to be bound by
  * the terms of this license.
- * 
+ *
  * You must not remove this notice, or any other, from this software.
  */
 package clojure.contrib.jsr223;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringReader;
-import java.util.Map;
-
-import javax.script.AbstractScriptEngine;
-import javax.script.Bindings;
-import javax.script.Compilable;
-import javax.script.CompiledScript;
-import javax.script.Invocable;
-import javax.script.ScriptContext;
-import javax.script.ScriptEngineFactory;
-import javax.script.ScriptException;
-import javax.script.SimpleBindings;
-
 import clojure.lang.Compiler;
-import clojure.lang.IMapEntry;
-import clojure.lang.ISeq;
-import clojure.lang.LineNumberingPushbackReader;
-import clojure.lang.Namespace;
-import clojure.lang.RT;
-import clojure.lang.Symbol;
-import clojure.lang.Var;
+import clojure.lang.*;
+
+import javax.script.*;
+import java.io.*;
+import java.util.Map;
 
 /**
  * Implementation of a {@code ScriptEngine} for Clojure.
- * 
+ *
  * @author Armando Blancas
  * @version 1.2
  */
-class ClojureScriptEngine 
-	extends AbstractScriptEngine 
+class ClojureScriptEngine
+	extends AbstractScriptEngine
 	implements Invocable, Compilable {
 
 	private static final Symbol    USER_SYM            = Symbol.create("user");
@@ -56,17 +36,27 @@ class ClojureScriptEngine
 	private static final String    WARN_REFLECTION_KEY = "clojure.compile.warn-on-reflection";
 	private static final String    CLASSPATH           = System.getProperty("java.class.path");
 
+	//
+	//private static final String PATH_PROP = "clojure.compile.path";
+	//private static final String REFLECTION_WARNING_PROP = "clojure.compile.warn-on-reflection";
+	private static final String UNCHECKED_MATH_PROP = "clojure.compile.unchecked-math";
+
+	private static final Var compile_path = RT.var("clojure.core", "*compile-path*");
+	private static final Var compile = RT.var("clojure.core", "compile");
+	private static final Var warn_on_reflection = RT.var("clojure.core", "*warn-on-reflection*");
+	private static final Var unchecked_math = RT.var("clojure.core", "*unchecked-math*");
+
 	private final ScriptEngineFactory factory;
-	
+
 	/**
 	 * Default Constructor.
-	 * 
+	 *
 	 * @param sef The Script Engine Factory that created this instance.
 	 */
 	public ClojureScriptEngine(ScriptEngineFactory sef) {
 		if (sef == null)
 			throw new NullPointerException("factory is null");
-		
+
 		factory = sef;
 		Bindings engineScope = getBindings(ScriptContext.ENGINE_SCOPE);
 		engineScope.put(ENGINE, sef.getEngineName());
@@ -74,13 +64,13 @@ class ClojureScriptEngine
 		engineScope.put(NAME, sef.getEngineName());
 		engineScope.put(LANGUAGE, sef.getLanguageName());
 		engineScope.put(LANGUAGE_VERSION, sef.getLanguageVersion());
-		
+
 		// Defaults used for compiling Clojure sources.
 		engineScope.put(SOURCE_PATH_KEY, null);
 		engineScope.put(COMPILE_PATH_KEY, "classes");
 		engineScope.put(WARN_REFLECTION_KEY, Boolean.valueOf(false));
 	}
-	
+
 	/*
 	 * Bindings are interned according to the format namespace/var,
 	 * or user/var if only the var is given.
@@ -94,7 +84,7 @@ class ClojureScriptEngine
 		    		String[] names = key.split("/");
 		    		nsName = names[0];
 		    		key = names[1];
-		    	} 
+		    	}
 		    	Object value = entry.getValue();
 		    	Var.intern(Namespace.findOrCreate(Symbol.create(nsName.intern())), Symbol.create(key.intern()), value);
 		    }
@@ -109,7 +99,7 @@ class ClojureScriptEngine
             Namespace ns = (Namespace) seq.first();
             String nsName = ns.toString();
             if (nsName.startsWith("clojure")) continue;
-            
+
 	        for (ISeq mseq = ns.getMappings().seq(); mseq != null; mseq = mseq.next()) {
 	            IMapEntry e = (IMapEntry) mseq.first();
 	            String k = e.getKey().toString();
@@ -118,11 +108,11 @@ class ClojureScriptEngine
             	if (val instanceof Var) {
             		val = ((Var) val).deref();
                 	bindings.put(nsName + "/" + k, val);
-            	}       	
+            	}
 	        }
         }
     }
-    
+
 	/**
 	 * {@inheritDoc}
 	 * <p>
@@ -134,45 +124,45 @@ class ClojureScriptEngine
 
 	/**
 	 * {@inheritDoc}
-	 * <p> 
+	 * <p>
 	 * The Clojure runtime will keep its state between {@code eval()} calls.
 	 * Previous to running the script:
 	 * <p>
 	 * 1- All Engine and Global bindings are applied to the {@code user} namespace.
 	 * <p>
-	 * 2- The Clojure standard streams {@code*in*}, {@code *out*} and {@code *err*} are 
+	 * 2- The Clojure standard streams {@code*in*}, {@code *out*} and {@code *err*} are
 	 *    redirected to their corresponding values in the passed {@code context}. The defaults
 	 *    are {@code System.in}, {@code System.out} and {@code System.err}, respectively.
 	 * <p>
 	 * 3- The Clojure runtime is set to the {@code user} namespace in order to provide
 	 *    consistency with the REPL.
 	 * <p>
-	 * For consistency with the REPL, redirect {@code *err* } to a {@code PrintWriter}.  
+	 * For consistency with the REPL, redirect {@code *err* } to a {@code PrintWriter}.
 	 */
 	public Object eval(String script, ScriptContext context)
 	throws ScriptException {
 		if (script == null)
 			throw new NullPointerException("script is null");
-		
+
 		return eval(new StringReader(script), context);
 	}
 
 	/**
 	 * {@inheritDoc}
-	 * <p> 
+	 * <p>
 	 * The Clojure runtime will keep its state between {@code eval()} calls.
 	 * Previous to running the script:
 	 * <p>
 	 * 1- All Engine and Global bindings are applied to the {@code user} namespace.
 	 * <p>
-	 * 2- The Clojure standard streams {@code*in*}, {@code *out*} and {@code *err*} are 
+	 * 2- The Clojure standard streams {@code*in*}, {@code *out*} and {@code *err*} are
 	 *    redirected to their corresponding values in the passed {@code context}. The defaults
 	 *    are {@code System.in}, {@code System.out} and {@code System.err}, respectively.
 	 * <p>
 	 * 3- The Clojure runtime is set to the {@code user} namespace in order to provide
 	 *    consistency with the REPL.
 	 * <p>
-	 * For consistency with the REPL, redirect {@code *err* } to a {@code PrintWriter}.  
+	 * For consistency with the REPL, redirect {@code *err* } to a {@code PrintWriter}.
 	 */
 	public Object eval(Reader reader, ScriptContext context)
 	throws ScriptException {
@@ -182,33 +172,33 @@ class ClojureScriptEngine
 			throw new NullPointerException("context is null");
 
 		Object result = null;
-		
+
 		try {
 			Bindings globalScope = context.getBindings(ScriptContext.GLOBAL_SCOPE);
 			if (globalScope != null)
 				applyBindings(globalScope);
-			
+
 			Bindings engineScope = context.getBindings(ScriptContext.ENGINE_SCOPE);
 			if (engineScope != null)
 				applyBindings(engineScope);
-			
+
 			Var.pushThreadBindings(
 				RT.map(RT.CURRENT_NS, RT.CURRENT_NS.deref(),
 					   RT.IN, new LineNumberingPushbackReader(context.getReader()),
 					   RT.OUT, context.getWriter(),
 					   RT.ERR, context.getErrorWriter()));
-			
+
 			IN_NS.invoke(USER_SYM);
 			result = Compiler.load(reader);
 
             if (globalScope != null)
-                collectBindings(engineScope);            
+                collectBindings(engineScope);
 		} catch (Exception e) {
 			throw new ScriptException(e);
 		} finally {
 			Var.popThreadBindings();
 		}
-		
+
 		return result;
 	}
 
@@ -226,18 +216,18 @@ class ClojureScriptEngine
 	 *         Implementation of interface Invocable.                 *
 	 *                                                                *
 	 ******************************************************************/
-	
+
 	/**
 	 * {@inheritDoc}
 	 * <p>
 	 * To implement a Java interface use the {@code proxy} macro. This method
-	 * expects a var in the {@code user} namespace named after the interface 
+	 * expects a var in the {@code user} namespace named after the interface
 	 * with the suffix "Impl". For example, to implement an {@code ActionListener}
 	 * the Clojure code could be:
 	 * <pre>
 	 * (import java.awt.event.ActionListener)
-	 * 
-	 * (def ActionListenerImpl 
+	 *
+	 * (def ActionListenerImpl
 	 *   (proxy [ActionListener] []
 	 *     (actionPerformed [evt] (println "button pushed"))))
 	 * </pre>
@@ -250,7 +240,7 @@ class ClojureScriptEngine
 	public <T> T getInterface(Class<T> clasz) {
 		if (clasz == null)
 			throw new NullPointerException("clasz is null");
-		
+
 		String ns = "user";
 		Var var = RT.var(ns, clasz.getSimpleName()+"Impl");
 		return (var == null) ? null : (T) var.deref();
@@ -260,15 +250,15 @@ class ClojureScriptEngine
 	 * {@inheritDoc}
 	 * <p>
 	 * To implement a Java interface use the {@code proxy} macro. This method
-	 * looks for a var in the namespace named indicated by {@code thiz} and 
-	 * named after the interface with the suffix "Impl". For example, to 
+	 * looks for a var in the namespace named indicated by {@code thiz} and
+	 * named after the interface with the suffix "Impl". For example, to
 	 * implement an {@code ActionListener} in the {@code actions} namespace
 	 * the Clojure code could be:
 	 * <pre>
 	 * (ns actions
 	 *  (:import java.awt.event.ActionListener))
-	 *  
-	 * (def ActionListenerImpl 
+	 *
+	 * (def ActionListenerImpl
 	 *   (proxy [ActionListener] []
 	 *     (actionPerformed [evt] (println "button pushed"))))
 	 * </pre>
@@ -295,35 +285,35 @@ class ClojureScriptEngine
 	 * {@inheritDoc}
 	 * <p>
 	 * If the function name is not qualified with a namespace, this method
-	 * looks for it in the {@code user} namespace, thus {@code foo} is 
+	 * looks for it in the {@code user} namespace, thus {@code foo} is
 	 * equivalent to {@code user/foo}. Functions in other namespaces must
 	 * used their fully-qualified names.
 	 * <p>
 	 * As in the {@code eval()} calls, bindings and redirections are applied
-	 * prior to invoking the function. 
+	 * prior to invoking the function.
 	 */
 	public Object invokeFunction(String name, Object... args)
 	throws ScriptException, NoSuchMethodException {
 		if (name == null)
 			throw new NullPointerException("name is null");
-		
+
 		Object result = null;
 		String format = "Function %s not found in namespace %s";
-		
+
 		try {
 			Bindings globalScope = context.getBindings(ScriptContext.GLOBAL_SCOPE);
 			if (globalScope != null)
 				applyBindings(globalScope);
-			
+
 			Bindings engineScope = context.getBindings(ScriptContext.ENGINE_SCOPE);
 			if (engineScope != null)
 				applyBindings(engineScope);
-			
+
 			Var.pushThreadBindings(
 				RT.map(RT.CURRENT_NS, RT.CURRENT_NS.deref(),
 					   RT.IN, new LineNumberingPushbackReader(context.getReader()),
 					   RT.OUT, context.getWriter(),
-					   RT.ERR, context.getErrorWriter()));		
+					   RT.ERR, context.getErrorWriter()));
 
 			if (name.indexOf('/') == -1) {
 				String ns = "user";
@@ -342,15 +332,15 @@ class ClojureScriptEngine
 			    }
 				result = var.applyTo(RT.seq(args));
 			}
-			
+
             if (globalScope != null)
-                collectBindings(engineScope);            
+                collectBindings(engineScope);
 		} catch (Exception e) {
 			throw new ScriptException(e);
 		} finally {
 			Var.popThreadBindings();
 		}
-		
+
 		return result;
 	}
 
@@ -381,72 +371,46 @@ class ClojureScriptEngine
 	 * <p>
 	 * This engine will recognize and pass on these properties to the Clojure
 	 * compiler:
-	 * <p> 
-	 * {@code clojure.source.path} Additional locations of Clojure source files, 
+	 * <p>
+	 * {@code clojure.source.path} Additional locations of Clojure source files,
 	 * to be appended to the value of "java.class.path". This is an optional
 	 * property and defaults to {@code null}.
 	 * <p>
 	 * {@code clojure.compile.path} The location for the generated .class files.
 	 * Defaults to {@code "classes"}.
 	 * <p>
-	 * {@code clojure.compile.warn-on-reflection} Whether to get a warning when 
+	 * {@code clojure.compile.warn-on-reflection} Whether to get a warning when
 	 * Clojure will use Java reflection. Defaults to {@code Boolean false}.
 	 */
     public CompiledScript compile(String script)
     throws ScriptException {
 		if (script == null)
 			throw new NullPointerException("script is null");
-		
-		String library = script.trim();
+
+		final String library = script.trim();
 		if (library.length() == 0)
 			return null;
-		
-		try {
-			/*
-			 * Each compilation takes place in its own process with a clean
-			 * slate in the Clojure RT. No bindings nor redirections are
-			 * applied from the host Java code.
-			 */
-			StringBuffer classpath = new StringBuffer(CLASSPATH);
-			String cmp = (String) get(COMPILE_PATH_KEY);
-			if (cmp != null && cmp.length() > 0)
-				classpath.append(File.pathSeparatorChar).append(cmp);
-			String src = (String) get(SOURCE_PATH_KEY);
-			if (src != null && src.length() > 0)
-				classpath.append(File.pathSeparatorChar).append(src);
-			
-	    	String compile = 
-	    		String.format("java -D%s=%s -D%s=%b -cp %s clojure.lang.Compile %s",
-	    		    COMPILE_PATH_KEY, 
-	    			(String) get(COMPILE_PATH_KEY),
-	    		    WARN_REFLECTION_KEY, 
-	    		    (Boolean) get(WARN_REFLECTION_KEY),
-	    		    classpath.toString(),
-	    		    library);
-	    	
-			Process process = Runtime.getRuntime().exec(compile);
 
-			BufferedReader reader = 
-				new BufferedReader(
-                    new InputStreamReader(process.getErrorStream()));
-			StringBuffer buffer = new StringBuffer();
-            String line = reader.readLine();
-            while (line != null) {
-            	buffer.append(line).append('\n');
-            	line = reader.readLine();
-            }
-            reader.close();
-            
-	    	process.waitFor();
-	    	if (process.exitValue() != 0)
-	    		throw new ScriptException(buffer.toString());
-		} catch (IOException e) {
-			throw new ScriptException(e);
-		} catch (InterruptedException e) {
-			throw new ScriptException(e);
-		}
 
-		return null;
+		Var.pushThreadBindings(
+				RT.map(RT.CURRENT_NS, RT.CURRENT_NS.deref(),
+						RT.IN, new LineNumberingPushbackReader(context.getReader()),
+						RT.OUT, context.getWriter(),
+						RT.ERR, context.getErrorWriter()));
+
+		IN_NS.invoke(USER_SYM);
+
+		return new CompiledScript() {
+			@Override
+			public Object eval(ScriptContext context) throws ScriptException {
+				return ClojureScriptEngine.this.eval(library, context);
+			}
+
+			@Override
+			public ScriptEngine getEngine() {
+				return ClojureScriptEngine.this;
+			}
+		};
     }
 
 	/**
@@ -466,7 +430,7 @@ class ClojureScriptEngine
 			throw new NullPointerException("script is null");
 
 		BufferedReader bf = new BufferedReader(script);
-		
+
 		try {
 			String library = bf.readLine();
 			while (library != null) {
@@ -482,7 +446,7 @@ class ClojureScriptEngine
 				throw new ScriptException(e);
 			}
 		}
-		
+
 		return null;
     }
 
--- test/clojure/contrib/jsr223/ClojureScriptEngineTest.java
+++ test/clojure/contrib/jsr223/ClojureScriptEngineTest.java
@@ -1,70 +1,59 @@
 /*
  * Copyright (c) 2009 Armando Blancas. All rights reserved.
- * 
+ *
  * The use and distribution terms for this software are covered by the
  * Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
  * which can be found in the file epl-v10.html at the root of this distribution.
- * 
+ *
  * By using this software in any fashion, you are agreeing to be bound by
  * the terms of this license.
- * 
+ *
  * You must not remove this notice, or any other, from this software.
  */
 package clojure.contrib.jsr223;
 
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Reader;
-import java.io.StringReader;
 
-import javax.script.Bindings;
-import javax.script.Compilable;
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineFactory;
-import javax.script.ScriptException;
-import javax.script.SimpleBindings;
-
-import junit.framework.Assert;
 import junit.framework.JUnit4TestAdapter;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import javax.script.*;
+import java.awt.event.ActionListener;
+import java.io.*;
 
 public class ClojureScriptEngineTest {
 
 	/**
 	 * Runs the test suite in this class from the command line.
-	 * 
+	 *
 	 * @param args	Arguments are ignored.
 	 */
 	public static void main(String[] args) {
 		org.junit.runner.JUnitCore.runClasses(ClojureScriptEngineTest.class);
 	}
-	
+
 	/**
 	 * Provides compatibility with 3.x versions of JUnit.
-	 * 
+	 *
 	 * @return A 3.x-compatible test suite.
 	 */
 	public static junit.framework.Test suite() {
 		return new JUnit4TestAdapter(ClojureScriptEngineTest.class);
 	}
-	
+
     /*+----------------------------------------------------------------------+
-	  |                                                                      | 
-	  | Test the constructor.                                                | 
-	  |                                                                      | 
+	  |                                                                      |
+	  | Test the constructor.                                                |
+	  |                                                                      |
 	  +----------------------------------------------------------------------+*/
-	
+
 	@Test
 	public void fromFactory() {
 		ClojureScriptEngineFactory f = new ClojureScriptEngineFactory();
 		Assert.assertNotNull(f);
 		Assert.assertNotNull(f.getScriptEngine());
 	}
-	
+
 	@Test
 	public void fromFactoryInstanceClass() {
 		ScriptEngineFactory f = new ClojureScriptEngineFactory();
@@ -72,7 +61,7 @@ public class ClojureScriptEngineTest {
 		Assert.assertEquals(ClojureScriptEngineFactory.class, f.getClass());
 		Assert.assertEquals(ClojureScriptEngine.class, engine.getClass());
 	}
-	
+
 	@Test
 	public void constructor() {
 		ClojureScriptEngineFactory f = new ClojureScriptEngineFactory();
@@ -90,11 +79,11 @@ public class ClojureScriptEngineTest {
 	}
 
     /*+----------------------------------------------------------------------+
-	  |                                                                      | 
-	  | Mainline tests.                                                      | 
-	  |                                                                      | 
+	  |                                                                      |
+	  | Mainline tests.                                                      |
+	  |                                                                      |
 	  +----------------------------------------------------------------------+*/
-	
+
 	@Test
 	public void bindings() {
 		ScriptEngine engine = new ClojureScriptEngineFactory().getScriptEngine();
@@ -102,21 +91,21 @@ public class ClojureScriptEngineTest {
 		Assert.assertNotNull(bindings);
 		Assert.assertEquals(SimpleBindings.class, bindings.getClass());
 	}
-	
+
 	@Test
 	public void eval_predef() throws ScriptException {
 		ScriptEngine engine = new ClojureScriptEngineFactory().getScriptEngine();
 		Object result = engine.eval("(+ 3 4)");
 		Assert.assertEquals(7L, result);
 	}
-	
+
 	@Test
 	public void eval_defn() throws ScriptException {
 		ScriptEngine engine = new ClojureScriptEngineFactory().getScriptEngine();
 		Object result = engine.eval("(defn mid [low high] (/ (+ low high) 2)) (mid 10 20)");
 		Assert.assertEquals(15L, result);
 	}
-	
+
 	@Test
 	public void eval_WithStringReader() throws ScriptException {
 		ScriptEngine engine = new ClojureScriptEngineFactory().getScriptEngine();
@@ -124,7 +113,7 @@ public class ClojureScriptEngineTest {
 		Object result = engine.eval(r);
 		Assert.assertEquals(15L, result);
 	}
-	
+
 	@Test
 	public void eval_WithFileReader() throws ScriptException, FileNotFoundException {
 		ScriptEngine engine = new ClojureScriptEngineFactory().getScriptEngine();
@@ -132,7 +121,7 @@ public class ClojureScriptEngineTest {
 		Object result = engine.eval(r);
 		Assert.assertEquals(200L, result);
 	}
-	
+
 	@Test
 	public void eval_WithBinding() throws ScriptException {
         File file = new File("./scripts/converter.clj");
@@ -142,78 +131,88 @@ public class ClojureScriptEngineTest {
 		String s = (String) engine.eval("(defn get-path [f] (.getPath f)) (get-path myFile)");
 		Assert.assertEquals("./scripts/converter.clj", s.replace(File.separatorChar, '/'));
 	}
-	
+
 	@Test
 	public void getInterface() throws ScriptException {
 		ScriptEngine engine = new ClojureScriptEngineFactory().getScriptEngine();
         engine.eval(
-        	"(import java.awt.event.ActionListener) " + 
+        	"(import java.awt.event.ActionListener) " +
         	"(def ActionListenerImpl                " +
         	"  (proxy [ActionListener] []           " +
-        	"    (actionPerformed [evt]             " + 
+        	"    (actionPerformed [evt]             " +
         	"      (println evt))))");
 
         Invocable inv = (Invocable) engine;
         ActionListener listener = inv.getInterface(ActionListener.class);
 		Assert.assertEquals(true, listener instanceof ActionListener);
 	}
-	
+
 	@Test
 	public void getInterface_WithNS() throws ScriptException {
 		ScriptEngine engine = new ClojureScriptEngineFactory().getScriptEngine();
         engine.eval(
         	"(ns foo-ns)                            " +
-        	"(import java.awt.event.ActionListener) " + 
+        	"(import java.awt.event.ActionListener) " +
         	"(def ActionListenerImpl                " +
         	"  (proxy [ActionListener] []           " +
-        	"    (actionPerformed [evt]             " + 
+        	"    (actionPerformed [evt]             " +
         	"      (println evt))))");
 
         Invocable inv = (Invocable) engine;
         ActionListener listener = inv.getInterface("foo-ns", ActionListener.class);
 		Assert.assertEquals(true, listener instanceof ActionListener);
 	}
-	
+
 	@Test
 	public void invokeFunction() throws ScriptException, NoSuchMethodException {
 		ScriptEngine engine = new ClojureScriptEngineFactory().getScriptEngine();
 		engine.eval("(defn mid [low high] (/ (+ low high) 2)) (mid 10 20)");
-		
+
         Invocable inv = (Invocable) engine;
         Object result = inv.invokeFunction("mid", 10, 20);
 		Assert.assertEquals(15L, result);
 	}
-	
+
 	@Test
 	public void invokeFunction_WithNS() throws ScriptException, NoSuchMethodException {
 		ScriptEngine engine = new ClojureScriptEngineFactory().getScriptEngine();
 		engine.eval("(ns bar-ns)(defn middle [low high] (/ (+ low high) 2)) (middle 10 20)");
-		
+
         Invocable inv = (Invocable) engine;
         Object result = inv.invokeFunction("bar-ns/middle", 10, 20);
 		Assert.assertEquals(15L, result);
 	}
-	
+
 	@Test
 	public void invokeMethod() throws ScriptException, NoSuchMethodException {
 		ScriptEngine engine = new ClojureScriptEngineFactory().getScriptEngine();
 		engine.eval("(defn midpoint [low high] (/ (+ low high) 2)) (midpoint 10 20)");
-		
+
         Invocable inv = (Invocable) engine;
         Object result = inv.invokeMethod(null, "midpoint", 10, 20);
 		Assert.assertEquals(15L, result);
 	}
-	
+
 	@Test
 	public void invokeMethod_WithNS() throws ScriptException, NoSuchMethodException {
 		ScriptEngine engine = new ClojureScriptEngineFactory().getScriptEngine();
 		engine.eval("(ns bar-ns)(defn midpoint [low high] (/ (+ low high) 2)) (midpoint 10 20)");
-		
+
         Invocable inv = (Invocable) engine;
         Object result = inv.invokeMethod(null, "bar-ns/midpoint", 10, 20);
 		Assert.assertEquals(15L, result);
 	}
-/*	
+
+	@Test
+	public void compile_WithStringReader() throws ScriptException {
+		Compilable engine = (Compilable) new ClojureScriptEngineFactory().getScriptEngine();
+		String script = "(defn mid [low high] (/ (+ low high) 2)) (mid 10 20)";
+		CompiledScript result = engine.compile(script);
+
+		Assert.assertEquals(ClojureScriptEngine.class, result.getEngine().getClass());
+		Assert.assertEquals(15L, result.eval());
+	}
+/*
 	@Test
 	public void compileLibrary() throws ScriptException {
 		Delete delete = new Delete();
@@ -224,7 +223,7 @@ public class ClojureScriptEngineTest {
 		mkdir.execute();
 		File file = new File("classes/snake/snake_main.class");
 		Assert.assertFalse(file.exists());
-		
+
 		Compilable compile = (Compilable) new ClojureScriptEngineFactory().getScriptEngine();
 		compile.compile("snake.snake-main");
 		Assert.assertTrue(file.exists());
@@ -252,9 +251,9 @@ public class ClojureScriptEngineTest {
 	}
 */
 	/*+----------------------------------------------------------------------+
-	  |                                                                      | 
-	  | Negative tests.                                                      | 
-	  |                                                                      | 
+	  |                                                                      |
+	  | Negative tests.                                                      |
+	  |                                                                      |
 	  +----------------------------------------------------------------------+*/
 
 	@Test(expected=NullPointerException.class)
@@ -268,7 +267,7 @@ public class ClojureScriptEngineTest {
 		String script = null;
 		engine.eval(script);
 	}
-	
+
 	@Test(expected=NullPointerException.class)
 	public void eval_NullReader() throws ScriptException {
 		ScriptEngine engine = new ClojureScriptEngineFactory().getScriptEngine();
